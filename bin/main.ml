d6847db5bc823f7532b550c78ade1dde1c36fc67
robert <robertschmidt4th@gmail.com>
2024-07-26 17:05:27 +0200

    made repl look a bit nicer

--- bin/main.ml
+++ bin/main.ml
@@ -9,15 +9,16 @@ let count_char s c =
 
 let repl name env () =
   let rec aux buf =
-    print_string "mlisp ";
-    print_string name;
-    print_string " > ";
     Out_channel.(flush stdout);
     let buf = String.append buf In_channel.(input_line_exn stdin) in
     let opens = count_char buf '(' in
     let closes = count_char buf ')' in
     if closes >= opens then (
+      print_string "=> ";
       Parse.evaluate_program env buf;
+      print_string "mlisp ";
+      print_string name;
+      print_string " > ";
       aux buf)
     else aux buf
   in
@@ -27,12 +28,21 @@ let repl name env () =
 let () =
   let args = Sys.get_argv () in
   if Array.length args = 3 then (
-    if String.(args.(1) = "-r") then
+    if String.(args.(1) = "-r") then (
       let filename = args.(2) in
       let env = Parse.get_text filename () in
-      repl (String.drop_suffix filename 6) env ()
+      let name = String.drop_suffix filename 6 in
+      print_string "mlisp ";
+      print_string name;
+      print_string " > ";
+
+      repl name env ())
     else if String.(args.(1) = "-c") then
       let filename = args.(2) in
       let _ = Parse.get_text filename () in
       ())
-  else repl "" (Env.populate ()) ()
+  else (
+    print_string "mlisp ";
+    print_string "";
+    print_string " > ";
+    repl "" (Env.populate ()) ())
